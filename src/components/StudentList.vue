e854595fa17b004d1a87d134e7fb2e6cfb3d6722
Grgic Viktoria <grgic.viktoria@johak.at>
2022-11-29 11:18:35 +0100

    Create StudentList.vue

---
+++ src/components/StudentList.vue
@@ -0,0 +1,9 @@
+<script setup>
+import {ref} from 'vue'
+onMounted(async () =>{
+    const response = await fetch("https://pb.seiwald.club/api/collections/4bhk/records")
+})
+</script>
+<template> 
+<h1>OurStudents</h1>
+</template>
